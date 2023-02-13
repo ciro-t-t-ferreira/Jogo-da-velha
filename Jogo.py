7a75f04a3c04eaf0d5ccc89a968685e59f186e72
ciro-t-t-ferreira <ferreira.cirott@gmail.com>
2023-02-13 16:26:59 -0300

    Initial commit

---
+++ Jogo.py
@@ -0,0 +1,12 @@
+tabuleiro = [['x/y', '1', '2', '3'],['1', '_', '_', '_'],['2', '_', '_', '_'],['3', '_', '_', '_']]
+# Para selecionar uma posição no tabuleiro use <tabuleiro [x][y]>
+
+for x in tabuleiro:
+    print(x)
+
+
+#Lixeira
+# teste = [['1a', '1b', '1c', '1d'],['2a', '2b', '2c', '2d'],['3a', '3b', '3c', '3d'],['4a', '4b', '4c', '4d']]
+# for x in teste:
+#     print (x)
+# print(teste[1][2])
