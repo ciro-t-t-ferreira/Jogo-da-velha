93338a7eda8c49622919353d67bf60f8f708d9df
Ciro Ferreira <124924418+ciro-t-t-ferreira@users.noreply.github.com>
2023-02-14 16:12:07 -0300

    Add files via upload
    Finalização do projeto. Pode ser aperfeiçoado adicionando algo que impeça o usuário de entrar um valor inválido de coordenada (qualquer coisa que não seja um inteiro entre 1 e 3) e quebre o jogo

--- Jogo.py
+++ Jogo.py
@@ -1,12 +1,66 @@
+print("***************************")
+print("Bem vindo ao jogo da velha!")
+print("***************************")
+
 tabuleiro = [['x/y', '1', '2', '3'],['1', '_', '_', '_'],['2', '_', '_', '_'],['3', '_', '_', '_']]
-# Para selecionar uma posição no tabuleiro use <tabuleiro [x][y]>
 
 for x in tabuleiro:
     print(x)
 
+fechou = False
+vez = "X"
 
-#Lixeira
-# teste = [['1a', '1b', '1c', '1d'],['2a', '2b', '2c', '2d'],['3a', '3b', '3c', '3d'],['4a', '4b', '4c', '4d']]
-# for x in teste:
-#     print (x)
-# print(teste[1][2])
+while (not fechou):
+
+    print (f"Agora é vez de {vez}")
+    x = int (input ("Insira x (Entre 1 e 3)"))
+    y = int (input ("Insira y (Entre 1 e 3)"))
+
+    if (tabuleiro[x][y] == "_"): #Verifica se a casa já não foi marcada
+        tabuleiro[x][y] = vez
+    else:
+        print("Coordenada inválida")
+        continue
+
+    for x in tabuleiro:
+        print(x)
+
+    if (tabuleiro[1][1]==vez and tabuleiro[1][2]==vez and tabuleiro[1][3]==vez): #Condições de vitória
+        print(f"{vez} venceu!")
+        break
+    if (tabuleiro[2][1]==vez and tabuleiro[2][2]==vez and tabuleiro[2][3]==vez):
+        print(f"{vez} venceu!")
+        break
+    if (tabuleiro[3][1]==vez and tabuleiro[3][2]==vez and tabuleiro[3][3]==vez):
+        print(f"{vez} venceu!")
+        break
+    if (tabuleiro[1][1]==vez and tabuleiro[2][1]==vez and tabuleiro[3][1]==vez):
+        print(f"{vez} venceu!")
+        break
+    if (tabuleiro[1][2]==vez and tabuleiro[2][2]==vez and tabuleiro[3][2]==vez):
+        print(f"{vez} venceu!")
+        break
+    if (tabuleiro[1][3]==vez and tabuleiro[2][3]==vez and tabuleiro[3][3]==vez):
+        print(f"{vez} venceu!")
+        break
+    if (tabuleiro[1][1]==vez and tabuleiro[2][2]==vez and tabuleiro[3][3]==vez):
+        print(f"{vez} venceu!")
+        break
+    if (tabuleiro[1][3]==vez and tabuleiro[2][2]==vez and tabuleiro[3][1]==vez):
+        print(f"{vez} venceu!")
+        break                                                                   #Condições de vitória\
+
+    i = 0  # Condição que detecta empate
+    teste = ['_', '_', '_', '_']
+    for x in tabuleiro:
+        teste[i] = ("_" in x)
+        i += 1
+    if (not teste[1] and not teste[2] and not teste[3]):
+        print("Empate!")
+        fechou = True  # Condição que detecta empate\
+
+    if (vez == "X"): #Passa a vez
+        vez = "O"
+    else:
+        vez = "X"
+
